d3f68c1749051b834c644c060ed7fca2c80da14e
LuccaToniatto <90876239+LuccaToniatto@users.noreply.github.com>
2021-11-05 16:17:10 -0300

    Delete lavajato directory

--- lavajato/cripto.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $palavra = "SENAI";
-    echo $palavra."<br/>";
-    $cripto = base64_encode($palavra);
-    echo $cripto."<br/>";
-    $descripto = base64_decode($cripto);
-    echo $descripto;
-?>
